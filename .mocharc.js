bec3619e502c3a1fdc1ea844d96b9f98cab1a8ee
lanas22 <svetlana_gurina@hotmail.com>
2024-05-15 21:04:58 -0400

    add auth positive

--- .mocharc.js
+++ .mocharc.js
@@ -1,4 +1,5 @@
 module.exports - {
     require: ['@babel/register'],
     timeout: '20000',
+    exclude: [test/example.spec.js]
 };
---
+++ test/example.spec.js
@@ -0,0 +1,6 @@
+import { expect } from "chai"
+describe('VERIFY SIMPLE MATH FUNCTIONS', () => {
+    it('verify sum works properly', () => {
+        expect(4+4).equal(8);
+    });
+});
